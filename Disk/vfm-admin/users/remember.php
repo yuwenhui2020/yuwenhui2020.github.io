b8f63b3cbccebdc9c9e237e8e0687dad87812ad2
YuWenHui2020 <213785187@qq.com>
2021-03-11 15:13:49 +0800

    main
    delect

--- Disk/vfm-admin/users/remember.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- $_REMEMBER = array (
-  'user' => 'f6bc9aa3f57eea6b7cb193bf7a437fb5',
-  'root' => 'b0dd0b6ae47cf504bd328bb9f833009b',
-  'admin' => '1524db8ae61020e04db92647c7fef900',
-);
